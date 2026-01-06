ff1b0a187070059acd2af9e72896f535ba4304fb
kimds5344 <kimds5344@naver.com>
2026-01-06 09:22:51 +0900

    feat : sitemap.xml 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dskim.dev',
+	site: 'https://www.dskim.dev/',
 	integrations: [mdx(), sitemap()],
 });

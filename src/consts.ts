0372839b48e8ac78dab4543b4427597337c21cd5
kimds5344 <kimds5344@naver.com>
2026-01-06 19:49:25 +0900

    feat : 기능 수정

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '김대성의 블로그';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '백엔드 개발자 김대성의 블로그입니다.';

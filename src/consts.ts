1355692e91bcc381877e2c044328b86a37d01163
kimds5344 <kimds5344@naver.com>
2026-01-07 14:42:41 +0900

    feat : footer 하단 고정

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = '김대성의 블로그';
 export const SITE_DESCRIPTION = '백엔드 개발자 김대성의 블로그입니다.';
